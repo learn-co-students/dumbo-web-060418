5ed516fd7629a8eca44933d5f19dd11994908ad5
Tim Campbell <timothylevicampbell@gmail.com>
2018-07-19 17:47:33 -0400

    Update examples to not use arrow functions

--- 20-weather-api-fix/js/weatherChart.js
+++ 20-weather-api-fix/js/weatherChart.js
@@ -1,13 +1,19 @@
 function getFahrenheits(result) {
-  return result.hourly.data.map((t) => t.temperature);
+  return result.hourly.data.map(function(t) {
+    return t.temperature;
+  });
 }
 
 function getHours(result) {
-  return result.hourly.data.map((t) => new Date(t.time * 1000).getHours());
+  return result.hourly.data.map(function(t) {
+    return new Date(t.time * 1000).getHours();
+  });
 }
 
 function makeRequest(endpoint, success) {
   fetch(endpoint)
-    .then((r) => r.json())
+    .then(function(r) {
+      return r.json();
+    })
     .then(success);
 }
