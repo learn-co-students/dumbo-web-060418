d467bd2fc5959489cc3a36451a566709ebff6f52
nikymorg <nicole.morgan@gmail.com>
2018-08-10 12:30:11 -0400

    koala starter

---
+++ 27_component_lifecyle/src/components/RandomKoala.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class RandomKoala extends Component {
+
+  render () {
+    return (
+      <div className="app-children">
+
+      </div>
+    )
+  }
+}
+
+export default RandomKoala
