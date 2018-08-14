af7591480366f894629203fde7b366bd6a6288ee
nikymorg <nicole.morgan@gmail.com>
2018-08-14 14:54:12 -0400

    react review starter

---
+++ 28_react_review/src/components/AnimalCatalogue.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class AnimalCatalogue extends Component {
+  render() {
+    return (
+      <div>
+
+
+      </div>
+    );
+  }
+}
+
+export default AnimalCatalogue;
