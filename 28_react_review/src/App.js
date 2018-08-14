3cbecb1d993cc13a5f29fa8d171ea61e0cb6f0c0
nikymorg <nicole.morgan@gmail.com>
2018-08-14 14:56:15 -0400

    remove import

--- 28_react_review/src/App.js
+++ 28_react_review/src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import Zoo from 'Zoo.js'
-import AnimalCatalogue from 'AnimalCatalogue.js'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+
 
       </div>
     );
