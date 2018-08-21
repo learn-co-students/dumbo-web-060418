3e072cfc05e299f52f2fc25b32d441e6bece62b5
nikymorg <nicole.morgan@gmail.com>
2018-08-21 11:03:18 -0400

    detail

---
+++ 29_react_router/src/components/PaintingDetail.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const PaintingDetail = (props) => {
+
+  return (
+    <div>
+      <h1> Title: {painting.title} </h1>
+      <img alt={painting.title} src={painting.image} />
+      <h4> Date: {painting.date} </h4>
+      <p> Artist: {painting.artist.name} </p>
+      <p> Displayed in: {painting.museum.name} </p>
+    </div>
+  )
+}
+
+export default PaintingDetail
