56699c59a97e4802372e2c9242c331f807d0f896
nikymorg <nicole.morgan@gmail.com>
2018-08-21 09:16:23 -0400

    about

--- 29_react_router/src/components/About.js
+++ 29_react_router/src/components/About.js
@@ -4,7 +4,7 @@ const About = (props) => {
 
   return (
     <div>
-      <p>Niky is an artist who lives in Maine with her pet Dachsund and paints seascapes.</p>
+      <p>Niky is an artist who lives in Maine with her pet dachsund who paints seascapes. (The dachsund pains seascapes, not Niky.)</p>
       <img src='https://images-na.ssl-images-amazon.com/images/I/A1Zwubfyj3L._SX425_.jpg' />
     </div>
 
