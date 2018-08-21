8337d2becb15d97d638a0c3099eb3b20eb7acf17
nikymorg <nicole.morgan@gmail.com>
2018-08-21 09:14:49 -0400

    extra components

---
+++ 29_react_router/src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = (props) => {
+
+  return (
+    <div>
+      <p>Niky is an artist who lives in Maine with her pet Dachsund and paints seascapes.</p>
+      <img src='https://images-na.ssl-images-amazon.com/images/I/A1Zwubfyj3L._SX425_.jpg' />
+    </div>
+
+  )
+}
+
+export default About
---
+++ 29_react_router/src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Main = (props) => {
+
+  return (
+    <div>
+      This app does painting stuff.
+    </div>
+
+  )
+}
+
+export default Main
